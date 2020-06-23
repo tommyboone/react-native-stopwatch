f4eb277832ba863624bef7403b940a1dad09a7c5
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-06-23 11:24:33 -0500

    style updates

--- Components/Stopwatch.js
+++ Components/Stopwatch.js
@@ -8,7 +8,7 @@ import {
 } from 'react-native';
 import {Stopwatch} from 'react-native-stopwatch-timer';
 
-const image = { uri: "https://reactjs.org/logo-og.png" }; 
+const image = {uri: 'https://reactjs.org/logo-og.png'};
 
 export default class BasicStopwatch extends Component {
   constructor(props) {
@@ -35,49 +35,49 @@ export default class BasicStopwatch extends Component {
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <ImageBackground source={image} style={{width: "100%", height: "100%"}} >
-        <View
-          style={{
-            flex: 1,
-            marginTop: 32,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundImage: '',
-          }}>
-          <Stopwatch
-            laps
-            msecs
-            start={this.state.isStopwatchStart}
-            //To start
-            reset={this.state.resetStopwatch}
-            //To reset
-            options={options}
-            //options for the styling
-            getTime={this.getFormattedTime}
-          />
-          <TouchableHighlight onPress={this.startStopStopWatch}>
-            <Text style={{fontSize: 20, marginTop: 10, color:"#fff"}}>
-              {!this.state.isStopwatchStart ? 'START' : 'STOP'}
-            </Text>
-          </TouchableHighlight>
-          <TouchableHighlight onPress={this.resetStopwatch}>
-            <Text style={{fontSize: 20, marginTop: 10, color:"#fff"}}>RESET</Text>
-          </TouchableHighlight>
-        </View>
+        <ImageBackground source={image} style={options.image}>
+          <View
+            style={{
+              flex: 1,
+              marginTop: 32,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundImage: '',
+            }}>
+            <Stopwatch
+              laps
+              start={this.state.isStopwatchStart}
+              //To start
+              reset={this.state.resetStopwatch}
+              //To reset
+              options={options}
+              //options for the styling
+              getTime={this.getFormattedTime}
+            />
+
+            <TouchableHighlight onPress={this.startStopStopWatch}>
+              <Text style={options.btn}>
+                {!this.state.isStopwatchStart ? 'START' : 'STOP'}
+              </Text>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={this.resetStopwatch}>
+              <Text style={options.btn}>RESET</Text>
+            </TouchableHighlight>
+          </View>
         </ImageBackground>
       </View>
     );
   }
 }
-const options = {
+const options = StyleSheet.create({
   container: {
     backgroundColor: 'darkslategray',
     padding: 5,
     borderRadius: 10,
-    width: "100%",
+    width: '95%',
     height: 100,
-
     alignItems: 'center',
+    
   },
   text: {
     fontSize: 60,
@@ -86,7 +86,22 @@ const options = {
   },
   image: {
     flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center"
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
-};
+  btn: {
+    backgroundColor: 'rebeccapurple',
+    fontSize: 20,
+    marginTop: 10,
+    color: '#fff',
+    width: 100,
+    height:30,
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: '#fff',
+    fontWeight: 'bold',
+    borderRadius: 5
+  },
+});
